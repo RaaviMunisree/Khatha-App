94c5c7a6567b88615a5b8df7bc92bffa2da05931
RaaviMunisree <130855389+RaaviMunisree@users.noreply.github.com>
2025-09-12 19:59:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 
 connectDB();
 
@@ -32,3 +32,4 @@ app.use("/payments", paymentRoutes);
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
